99f20a9e8ed8feca9db398f2ed51319ef8ade7e6
arnost.stelsovsky <arnost05@seznam.cz>
2022-09-16 12:06:08 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        intro();
+    }
+
+    static void intro() {
+        System.out.println("Zadejte vetu:");
+        String userInputSentence = sc.nextLine();
+        System.out.println("Zadejte serii znaku");
+        String userInputCharseq = sc.nextLine();
+        analisis(userInputSentence, userInputCharseq);
+    }
+
+
+    static void analisis(String userInputSentence, String userInputCharseq) {
+        String[] splitted = userInputSentence.split(" ");
+        int count = 0;
+        for (String s : splitted) {
+            if (s.equals(userInputCharseq)){
+                count++;
+            }
+        }
+        System.out.println("Veta obsahuje tuto sekvenci "+count+"krat.");
+    }
+}
